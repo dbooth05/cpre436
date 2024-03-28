a4c76386a97ba12d2a946db9a6525f98fa210797
Dylan <dbooth05@iastate.edu>
2024-03-27 21:46:34 -0500

    temp upload

---
+++ website/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import HeaderFooter from './HeaderFooter.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <HeaderFooter />
+);
+
+let mainArea = document.getElementById('main');
+
+
